20e5ef05b524be798965eb1b9043f0c5ca1f3ee1
Vivian Cromwell <vivian@getchop.io>
2017-01-29 20:04:46 -0800

    fixed param error
    run ex2.  done.

--- homework_wk3/ex2/plotData.m
+++ homework_wk3/ex2/plotData.m
@@ -14,16 +14,12 @@ figure; hold on;
 
 % Find Indices of Positive and Negative examples
 pos = find(y==1);
-neg = find(y==0);
+neg = find(y == 0);
 
 % Plot examples
 
-plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, ... 'MarkerSize', 7);
-
-plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...'MarkerSize',7);
-
-
-
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize',7);
 
 
 
