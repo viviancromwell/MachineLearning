cfcaeb38f5247cea52506d41a645029bfeeee020
Vivian Cromwell <vivian@getchop.io>
2017-03-08 09:19:02 -0800

    regularization for cost function and gradient
    i love die hard. oh yes baby.

--- homework_wk9/ex8/cofiCostFunc.m
+++ homework_wk9/ex8/cofiCostFunc.m
@@ -45,7 +45,7 @@ Theta_grad = zeros(size(Theta));
 
 prediction = X * Theta';
 r = (prediction - Y) .* R;
-J = sum(sum(r .^ 2)) + lambda * (sum(sum(Theta .^ 2))/2 + sum(sum(X .^ 2)) )/2;
+J = 0.5 * sum(sum(r .^ 2)) + 0.5 * lambda * ( sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)) );
 X_grad = r * Theta + lambda * X;
 Theta_grad = r' * X + lambda * Theta;
 
